e47bd49986bd9b393fc36bdc24c2c9b3fd136bdd
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2023-03-22 16:05:32 +0100

    Remove unnecessary comments from IList interface

--- src/Lista03/Zad1/IList.java
+++ src/Lista03/Zad1/IList.java
@@ -1,15 +1,15 @@
 package Lista03.Zad1;
 
 public interface IList<E> extends Iterable<E> {
-    boolean add(E e); // dodanie elementu na koniec listy
-    void add(int index, E element); // dodanie elementu na podanej pozycji
-    void clear(); // skasowanie wszystkich elementów
-    boolean contains(E element); // czy lista zawiera podany element (equals())
-    E get(int index); // pobranie elementu z podanej pozycji
-    E set(int index, E element); // ustawienie nowej wartości na pozycji
-    int indexOf(E element); // pozycja szukanego elementu (equals())
-    boolean isEmpty(); // czy lista jest pusta
-    E remove(int index); // usuwa element z podanej pozycji
-    boolean remove(E element); // usuwa element (equals())
-    int size(); // rozmiar listy
+    boolean add(E e);
+    void add(int index, E element);
+    void clear();
+    boolean contains(E element);
+    E get(int index);
+    E set(int index, E element);
+    int indexOf(E element);
+    boolean isEmpty();
+    E remove(int index);
+    boolean remove(E element);
+    int size();
 }
